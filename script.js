873146a171a7afd95ee4e79e41259919e3d42432
Venator71 <b.johnhunter71@gmail.com>
2022-03-14 09:22:06 -0500

    fixed issues and added more options. made more comments

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ console.log(disneyPlusShows);
 const netFlixMovies = {
     action: "Extraction",
     crime: "The Irishman" 
-}
+};
 
 // 2b
 
 const amazonPrimeMovies = {
     action: "The Tomorrow War ",
     drama: "One Night in Miami"
-}
+};
 
 // 2c
 
@@ -42,7 +42,7 @@ const disneyJunior = ["Mickey Mouse Clubhouse", "Spidey and His Amazing Friends"
 
 //3b
 
-const [mickey, ...spidey] = disneyJunior;
+const [mickey, spidey] = disneyJunior;
 console.log(disneyJunior);
 
 // 3c
@@ -54,7 +54,7 @@ console.log(mickey, spidey);
 const avengers = {
     warMachine: 'James Rhodes',
     theHulk: 'Bruce Banner'
-}
+};
 
 // 4b
 
@@ -75,7 +75,7 @@ const moreAvengers = {
 
 // 4e
 
-const {nat, ...others} = moreAvengers;
+const {blackWidow:nat, ...others} = moreAvengers; //Needs the key because they are not an array they are key:value pairs.
 
 // 4f
 
@@ -88,15 +88,35 @@ const bonus = {
     third: [7, 8, 9]
   };
 
-  // 5a
+// 5a
   const {first, second, third}= bonus //turning key:value pairs into variables
   const all = [...first, ...second, ...third]; //combining using spread  
   console.log(all); // verifying/testing that it worked the way I intended
 
+/* another way from Jamie Flynt 
+    const all = [
+                ...bonus["first"],
+                ...bonus["second"],
+                ...bonus["third"]
+    ];
+*/
+// or
+/*
+const = all [
+    ...bonus.first,
+    ...bonus.second,
+    ...bonus.third
+];
+*/
 
 // 5b
-  const [one, two, three, four, five, six, seven, eight, nine] = all; // deconstruction and pairing values
-  
+  //const [one, two, three, four, five, six, seven, eight, nine] = all; // deconstruction and pairing values
+  // or
+
+const [one, two, three] = bonus.first;
+const [four, five, six] = bonus.second;
+const [seven, eight, nine] =bonus.third;
+
   // Now to verify it worked
   console.log(one);
   console.log(two);
@@ -108,3 +128,6 @@ const bonus = {
   console.log(eight);
   console.log(nine);
   
+  // or if you want it all on one line.
+
+  console.log(one, two, three, four, five, six, seven, eight, nine);
